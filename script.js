11c7ac6cc63a45da544fd7b356c8c1bc6075fb4b
SimonIsCoding <108285719+SimonIsCoding@users.noreply.github.com>
2025-11-02 14:45:41 +0800

    adding logs

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ button.addEventListener('click', () => {
 
 async function sendSearch()
 {
+	console.log('enter in sendSearch');
 	const query = document.getElementById('search').value;
+	console.log(`query = _${query}_`);
 	await fetch('https://n8n.srv1076432.hstgr.cloud/webhook/search', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
