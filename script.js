965a0d214779a1dbb942d77f1f2fcce395e505c6
SimonIsCoding <108285719+SimonIsCoding@users.noreply.github.com>
2025-11-02 13:14:33 +0800

    send query

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const input = document.getElementById('search');
 button.addEventListener('click', () => {
 	alert('Tu as cherché : ' + input.value);
 });
+
+async function sendSearch()
+{
+	const query = document.getElementById('search').value;
+	await fetch('https://n8n.srv1076432.hstgr.cloud/webhook/search', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ query })
+	});
+}
+
