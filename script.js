f5fc7622c79c8899e930c3aa3735102d198e6cb8
SimonIsCoding <108285719+SimonIsCoding@users.noreply.github.com>
2025-11-02 13:15:52 +0800

    change msg

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const button = document.getElementById('submit');
 const input = document.getElementById('search');
 
 button.addEventListener('click', () => {
-	alert('Tu as cherché : ' + input.value);
+	alert('Merci. Tu as cherché : ' + input.value);
 });
 
 async function sendSearch()
